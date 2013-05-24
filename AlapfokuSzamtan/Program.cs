0602eb6fb898b2771d3bc8a75ba3cfb4661adb26
Tóth Marcell Dávid <marcelldavid96@freemail.hu>
2013-05-24 21:51:01 +0200

    Alapművelet. :)

---
+++ AlapfokuSzamtan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlapfokuSzamtan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        #region
+        Console.WriteLine("Hello, a nevem Alapfokú Számtan!");
+        Console.ReadLine();
+        Console.WriteLine("Add meg az első számot!");
+        int elsoSzam = int.Parse(Console.ReadLine());
+        Console.WriteLine("Add meg a második számot!");
+        int masodikSzam = int.Parse(Console.ReadLine());
+        Console.WriteLine("Válaszd meg az elvégzendő műveletet! (+, -, *, /)");
+        string muveletiJel = Console.ReadLine();
+        
+        /* sztringValtozoNeve.ElementAt(<hanyadik karaktert szeretnéd>);
+         * Az adott indexen szereplő karaktert adja vissza.
+         * 
+         * FONTOS: A stringek indexe 0-ról indul!
+         */
+
+        Console.WriteLine(muveletiJel.ElementAt(0));
+
+        Console.ReadLine();
+        #endregion
+        }
+    }
+}

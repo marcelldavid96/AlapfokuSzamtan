edda1cfc667dca38dcbb035d855bc1e4aea22ca2
Tóth Marcell Dávid <marcelldavid96@freemail.hu>
2013-05-24 22:18:01 +0200

    Négy alapművelet. ;)

--- AlapfokuSzamtan/Program.cs
+++ AlapfokuSzamtan/Program.cs
@@ -9,26 +9,64 @@ namespace AlapfokuSzamtan
     {
         static void Main(string[] args)
         {
-        #region
-        Console.WriteLine("Hello, a nevem Alapfokú Számtan!");
-        Console.ReadLine();
-        Console.WriteLine("Add meg az első számot!");
-        int elsoSzam = int.Parse(Console.ReadLine());
-        Console.WriteLine("Add meg a második számot!");
-        int masodikSzam = int.Parse(Console.ReadLine());
-        Console.WriteLine("Válaszd meg az elvégzendő műveletet! (+, -, *, /)");
-        string muveletiJel = Console.ReadLine();
-        
-        /* sztringValtozoNeve.ElementAt(<hanyadik karaktert szeretnéd>);
-         * Az adott indexen szereplő karaktert adja vissza.
-         * 
-         * FONTOS: A stringek indexe 0-ról indul!
-         */
+            /* sztringValtozoNeve.ElementAt(<hanyadik karaktert szeretnéd>);
+             * Az adott indexen szereplő karaktert adja vissza.
+             * FONTOS: A stringek indexe 0-ról indul!
+             */
 
-        Console.WriteLine(muveletiJel.ElementAt(0));
+            #region
+            Console.WriteLine("Hello, a nevem Alapfokú Számtan!");
+            Console.ReadLine();
+            Console.WriteLine("Add meg az első számot!");
+            double elsoSzam = double.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a második számot!");
+            double masodikSzam = double.Parse(Console.ReadLine());
+            Console.WriteLine("Válaszd meg az elvégzendő műveletet! (+, -, *, /)");
+            string muveletiJel = Console.ReadLine();
+            if (muveletiJel.ElementAt(0) == '+')
+            {
+                // ez akkor fut le, ha a megadott műveleti jel a '+'
+                Console.WriteLine(elsoSzam + masodikSzam);
+            }
+            if (muveletiJel.ElementAt(0) == '-')
+            {
+                Console.WriteLine(elsoSzam - masodikSzam);
+            }
+            if (muveletiJel.ElementAt(0) == '*')
+            {
+                Console.WriteLine(elsoSzam * masodikSzam);
+            }
+            if (muveletiJel.ElementAt(0) == '/')
+            {
+                Console.WriteLine(elsoSzam / masodikSzam);
+            }
+            Console.ReadLine();
+            #endregion
 
-        Console.ReadLine();
-        #endregion
+            /* = : értékadás:           int valtozo = 2; (a valtozo legyen egyenlő kettővel)
+             * == : összehasonlítás:    valtozo1 == valtozo2 (valtozo1 egyenlő -e valtozo2-vel => vagy IGEN, vagy NEM (if))
+             * 
+             * "If" minta (feltételes elágazás)
+             * 
+             * Ha az feltétel értéke igaz, akkor a küvetkező blokkban levő utasításokat hajtja végre.
+             * Ha hamis, akkor a különben (else) blokkra ugrik.
+             * Ilyen az if blokk utasításai nem hajtódnak végre.
+             * Mert a feltétel vagy IGAZ VAGY HAMIS (true or false)!
+             * 
+             * <code>
+             * if (feltétel)
+             * {
+             *     utasítás1;
+             *     utasítás2;
+             * }
+             * else
+             * {
+             *      utasítás3;
+             *      utasítás4;
+             * }
+             * </code>
+             */
+
         }
     }
 }
